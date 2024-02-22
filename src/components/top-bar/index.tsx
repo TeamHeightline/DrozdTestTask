27f08d5ba38647b7b306e427a89f6b7172ba96dd
t.chirtsov <teamheightline@mail.ru>
2024-02-22 13:51:46 +0300

    Сделал чтобы на телефоне меню превращалось в кнопку и выезжающий список

--- src/components/top-bar/index.tsx
+++ src/components/top-bar/index.tsx
@@ -1,41 +1,50 @@
+import React, {useState} from 'react';
 import Logo from "@/src/components/logo";
 import {useAuth} from "@/src/context/auth-context";
 
 const pages = [
     {
         url: "/quiz",
-        title: "Викторины"
+        title: "Викторины",
     },
     {
         url: "/users",
-        title: "Пользователи"
+        title: "Пользователи",
     },
     {
         url: "/login",
-        title: "Войти"
+        title: "Войти",
     },
     {
         url: "/logout",
-        title: "Выйти"
+        title: "Выйти",
     },
-]
+];
+
 export default function TopBar() {
     const {isAuthenticated} = useAuth();
+    const [isOpen, setIsOpen] = useState(false);
 
     const availablePages = pages.filter((item) =>
-        item.url !== (isAuthenticated ? "/login" : "/logout"))
+        item.url !== (isAuthenticated ? "/login" : "/logout"));
 
     return (
-        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-b dark:border-gray-700 ">
+        <nav className="bg-white border-gray-200 dark:bg-gray-800 dark:border-b dark:border-gray-700">
             <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
                 <Logo/>
-                <div className="hidden w-full md:block md:w-auto" id="navbar-default">
-                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:border-0 dark:border-gray-700">
+                <button className="md:hidden p-2" onClick={() => setIsOpen(!isOpen)}>
+                    <svg className="w-6 h-6" fill="none" stroke="currentColor" viewBox="0 0 24 24"
+                         xmlns="http://www.w3.org/2000/svg">
+                        <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2"
+                              d={`M4 6h16M4 12h16m-7 6h7${isOpen ? "M4 18h.01" : ""}`}></path>
+                    </svg>
+                </button>
+                <div className={`${isOpen ? 'block' : 'hidden'} w-full md:block md:w-auto`} id="navbar-default">
+                    <ul className="font-medium flex flex-col p-4 md:p-0 mt-4 border border-gray-100 rounded-lg md:flex-row md:space-x-8 rtl:space-x-reverse md:mt-0 md:text-sm md:font-medium md:border-0 md:bg-white dark:bg-gray-800 dark:border-gray-700">
                         {availablePages.map((route) => (
                             <li key={route.url}>
                                 <a href={route.url}
-                                   className={`block py-2 px-3 rounded md:p-0 md:border-0 text-gray-900 dark:text-white dark:hover:text-primary-500`}
-                                >
+                                   className="block py-2 px-3 rounded md:p-0 md:border-0 text-gray-900 dark:text-white dark:hover:text-primary-500">
                                     {route.title}
                                 </a>
                             </li>

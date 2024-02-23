35431a72669132ec0e223d9cd2fa8b52f8cef9b8
timofei <teamheightline@mail.ru>
2024-02-23 10:44:55 +0300

    Сделал вывод не ID квиза, а название

--- src/pages/my-statistic/index.tsx
+++ src/pages/my-statistic/index.tsx
@@ -1,4 +1,6 @@
 import React, { useEffect, useState } from 'react';
+import {GetServerSideProps} from "next";
+import {Quiz} from "@/src/types/quiz";
 
 interface Statistic {
     id: string;
@@ -6,7 +8,25 @@ interface Statistic {
     correctAnswers: string[];
 
 }
-export default function MyStatistic()  {
+
+type QuizzesProps = {
+    quizzes: Quiz[];
+};
+
+export const getServerSideProps: GetServerSideProps<QuizzesProps> = async () => {
+    const baseUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+    const questionsUrl = "/quiz";
+    const res = await fetch(baseUrl + questionsUrl);
+
+    if (!res.ok) {
+        throw new Error('Failed to fetch data');
+    }
+    const quizzes: Quiz[] = await res.json();
+
+    return {props: {quizzes}};
+};
+
+export default function MyStatistic({quizzes}: QuizzesProps)  {
     const [statistics, setStatistics] = useState<Statistic[]>([]);
 
     useEffect(() => {
@@ -27,19 +47,22 @@ export default function MyStatistic()  {
                 <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
                     <thead className="text-xs text-gray-700 uppercase bg-gray-50 dark:bg-gray-700 dark:text-gray-400">
                     <tr>
-                        <th className="py-3 px-2">ID Квиза</th>
+                        <th className="py-3 px-2">Квиз</th>
                         <th className="py-3 px-2">Попыток</th>
                         <th className="py-3 px-2">Число правильных ответов</th>
                     </tr>
                     </thead>
                     <tbody className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                    {statistics.map((stat) => (
+                    {statistics.map((stat) => {
+                        const quiz = quizzes.find((item) => String(item.id) === stat.id)
+                        return(
                         <tr key={stat.id} className="bg-white border-b dark:bg-gray-800 dark:border-gray-700">
-                            <td className="px-6 py-4">{stat.id}</td>
+                            <td className="px-6 py-4">{quiz?.name || ""}</td>
                             <td className="px-6 py-4">{stat.attempts}</td>
                             <td className="px-6 py-4">{stat.correctAnswers.join(', ')}</td>
                         </tr>
-                    ))}
+                    )}
+                    )}
                     </tbody>
                 </table>
             </div>
